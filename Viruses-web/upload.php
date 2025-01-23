f96487f24c9d774791008b09c9697031810003e6
ivanbahe <117343556+ivanbahe@users.noreply.github.com>
2025-01-23 19:34:51 +0100

    Update upload.php (para el scan de carpetas)

--- Viruses-web/upload.php
+++ Viruses-web/upload.php
@@ -7,16 +7,22 @@ $pythonScript = '/var/www/html/bueno.py';
 
 if (!empty($_FILES['files']['name'][0])) {
     foreach ($_FILES['files']['name'] as $key => $fileName) {
+        $filePath = $targetDir . $fileName;
+        $directory = dirname($filePath);
+        
+        // Crear directorios si no existen
+        if (!file_exists($directory)) {
+            mkdir($directory, 0777, true);
+        }
 
-        $targetFilePath = $targetDir . basename($fileName);
-
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$key], $targetFilePath)) {
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$key], $filePath)) {
             echo "El archivo $fileName se ha subido correctamente.<br>";
-
-            // Ejecutar el script y capturar la salida estándar y los errores
-            $command = escapeshellcmd("python3 $pythonScript $targetFilePath 2>&1");
+            
+            // Ejecutar script Python
+            $cleanPath = escapeshellarg($filePath);
+            $command = "python3 $pythonScript $cleanPath 2>&1";
             $output = shell_exec($command);
-
+            
             echo "<pre>$output</pre>";
         } else {
             echo "Error al subir el archivo $fileName.<br>";
