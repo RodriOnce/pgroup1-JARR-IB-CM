2845ec4d6d8cb0a0be61a8d288131e78fd58c2ea
ivanbahe <117343556+ivanbahe@users.noreply.github.com>
2025-01-23 20:06:05 +0100

    Update index.js

--- Viruses-web/index.js
+++ Viruses-web/index.js
@@ -12,8 +12,8 @@ async function checkURL() {
         const apiKey = "446f02363118eb9dc67c1c250a5eaacd971f6ccf4a26b93b6a07b175bbcc4777";
         const apiUrl = `https://www.virustotal.com/api/v3/urls`;
 
-        // Convierte la URL a base64 como lo requiere VirusTotal
-        const encodedUrl = btoa(url);
+        // Convierte la URL a base64 sin relleno
+        const encodedUrl = btoa(url).replace(/=+$/, '');
 
         const response = await fetch(`${apiUrl}/${encodedUrl}`, {
             method: "GET",
@@ -28,7 +28,7 @@ async function checkURL() {
 
         const data = await response.json();
 
-        // Muestra los resultados (puedes personalizar esto según tus necesidades)
+        // Muestra los resultados
         if (data && data.data && data.data.attributes.last_analysis_stats) {
             const stats = data.data.attributes.last_analysis_stats;
             alert(`Resultado del análisis:
@@ -40,6 +40,6 @@ async function checkURL() {
         }
     } catch (error) {
         console.error("Error verificando la URL:", error);
-        alert("Ocurrió un error al verificar la URL.");
+        alert("Ocurrió un error al verificar la URL. Por favor, inténtelo de nuevo.");
     }
 }
