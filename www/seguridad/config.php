d313fa2b60e657a720198f94a05b095706ea9ff4
rodrionce <146068423+RodriOnce@users.noreply.github.com>
2025-05-25 23:52:24 +0200

    Add files via upload

---
+++ www/seguridad/config.php
@@ -0,0 +1,14 @@
+<?php
+/*------------------------------------------------------
+ |  Datos de conexión a la base de datos
+ *-----------------------------------------------------*/
+$dbHost = 'localhost';
+$dbName = 'empresa';
+$dbUser = 'root';
+$dbPass = 'momo';
+
+/*------------------------------------------------------
+ |  Carpeta donde se guardarán los ficheros de usuario
+ |  (seguimos usando la que ya existe dentro del docroot)
+ *-----------------------------------------------------*/
+$privatePath = '/var/www/html/archivos/';   //  <<--- RUTA ACTUAL
